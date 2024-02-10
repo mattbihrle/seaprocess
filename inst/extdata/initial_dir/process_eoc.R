b0a0baf248a6f5fb0b6ad5dcd4710257e2897eb5
Matt <matt.bihrle@gmail.com>
2024-02-10 16:48:10 -0600

    Edit process eoc to load in seaprocess library

--- inst/extdata/initial_dir/process_eoc.R
+++ inst/extdata/initial_dir/process_eoc.R
@@ -1,8 +1,12 @@
 #----------------EOC Processing-------------------------------------------------
 
 # This script is intended to setup the cruise project to be used easily
-# post-cruise. First, tell the function where to find raw cruise data. You should
-# be able to just copy and paste from "__process_data.R"
+# post-cruise. First, load the processing library
+
+library(seaprocess)
+
+#Then, tell the function where to find raw cruise data. You should
+# be able to just copy and paste from "<cruise_ID>_process_data.R"
 
 elg_folder <- "<enter-location-of-elg-folder>"
 ctd_folder <- "<enter-location-of-ctd-folder>"
@@ -20,16 +24,17 @@ eoc_process(elg_folder = elg_folder, ctd_folder = ctd_folder,
 
 # Then, overwrite the block of code pointing to the elg/adcp/ctd files on
 # different machines and tell the package to find them in the "raw/" directory.
-
-# Copy and paste the block below into "__process_data.R" and either delete or
-# comment out the old one
+#
+# Copy and paste the block below into "<cruise_ID>_process_data.R" and either
+# delete or comment out the old one
 
 elg_folder <- "raw/event"
 ctd_folder <- "raw/ctd"
 ros_folder <- "raw/ctd"
 adcp_folder <- "raw/adcp"
 
-# Finally, run through ALL of "__process_data.R" to ensure there are no issues.
+# Finally, run through ALL of "<cruise_ID>_process_data.R" once more to ensure
+# there are no issues.
 
 
 
