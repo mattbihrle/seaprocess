67589e7d41083c71b7a23ea1de5ee5d6c1ffebfe
mattbihrle <matt.bihrle@gmail.com>
2025-02-18 08:15:52 -0600

    Update name of session info file.

--- inst/extdata/initial_dir/eoc/session_info.R
+++ inst/extdata/initial_dir/eoc/session_info.R
@@ -1,7 +1,7 @@
 # Print Session Info------------------------------------------------------------
 
 # Run this line to log package versions used to analyze data in this script.
-sink("eoc/package_version_info.txt")
+sink("eoc/seaprocess_session_packages.txt")
 # Log System Time
 print("System Date and Time")
 print(Sys.time())
